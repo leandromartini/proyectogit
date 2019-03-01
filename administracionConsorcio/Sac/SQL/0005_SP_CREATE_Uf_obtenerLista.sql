7f0ae86e28985280efb342fe1caf284667385d01
leandro <leomartini@gmail.com>
2019-02-28 21:48:56 -0300

    sp_faltante

---
+++ administracionConsorcio/Sac/SQL/0005_SP_CREATE_Uf_obtenerLista.sql
@@ -0,0 +1,28 @@
+USE [SAR]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Uf_obtenerLista]    Script Date: 28/02/2019 21:47:15 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[Uf_obtenerLista] --1,-1,-1
+
+ @id_uf int = -1
+	        
+as
+
+select * from UF 
+
+
+
+GO
+
+
