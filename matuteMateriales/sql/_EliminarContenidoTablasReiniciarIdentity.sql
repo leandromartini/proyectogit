c79f7ef0bab856d9174576634d7172da4ab1b0e4
leandro <leomartini@gmail.com>
2019-07-10 23:25:24 -0300

    cambios App.config para conectar con servidor matuteMateriales
    cambio tabla para reset y inicializar a cero los identificadores

--- matuteMateriales/sql/_EliminarContenidoTablasReiniciarIdentity.sql
+++ matuteMateriales/sql/_EliminarContenidoTablasReiniciarIdentity.sql
@@ -8,11 +8,10 @@ delete from [dbo].[productosCantidad]
 delete from [dbo].[errores]
 DBCC CHECKIDENT ([errores], RESEED,0)
 
---delete from [dbo].[productos]
---DBCC CHECKIDENT ([productos], RESEED,0)
---delete from [dbo].[productosCantidad]
---DBCC CHECKIDENT ([productosCantidad], RESEED,0)
---delete from [dbo].[productosPrecio]
---DBCC CHECKIDENT ([productosPrecio], RESEED,0)
---delete from [dbo].[productosPrecioHist]
---DBCC CHECKIDENT ([productosPrecioHist], RESEED,0)
+delete from [dbo].[productos]
+DBCC CHECKIDENT ([productos], RESEED,0)
+
+delete from [dbo].[productosCantidad]
+delete from [dbo].[productosPrecio]
+delete from [dbo].[productosPrecioHist]
+
