a4863105cec35c2ca2be3bda8ad243575842f10c
Leandro <leandro.martini@cpba.com.ar>
2021-12-03 16:03:11 -0300

    comienzo el ejercicio 2

--- Ejercicios Javascript/arreglos.js
+++ Ejercicios Javascript/arreglos.js
@@ -1,8 +1,5 @@
+//Dada una hora en formato “HH:mm”, donde “HH” es un número entero de 0 a 11, y “mm” es un número entero de 0 a 59,
+// obtener los valores para los ángulos de la aguja de las horas y minutos. 
+//Debe considerarse el ángulo con respecto a la vertical del cuadrante del reloj, como se indica en la imagen:
+//Se espera como resultado de la llamada al programa / método / función los valores para las variables alfa (horas) y beta (minutos).
 
-
-
-function multiplicar(a){
-    return a;
-}
-
-console.log(multiplicar(5)(5))
